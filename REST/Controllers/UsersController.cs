f80b6c8d28815282d183c49725e26a84ab5f2d18
MyERP <tu.tran@yahoo.com>
2017-02-08 23:22:52 +0700

    add delete operation

--- REST/Controllers/UsersController.cs
+++ REST/Controllers/UsersController.cs
@@ -1,13 +1,15 @@
-﻿using REST.Models;
-using System;
-using System.Collections.Generic;
-using System.Web.Http;
+﻿
 namespace REST.Controllers
 {
+    using REST.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Http;
     [RoutePrefix("api/users")]
     public class UsersController : ApiController
     {
-        private static List<User> Users { get; set; }
+        private static IList<User> Users { get; set; }
         static UsersController()
         {
             UsersController.CreateTestUsers();
@@ -21,31 +23,56 @@ namespace REST.Controllers
         }
         [Route("")]
         [HttpGet()]
-        public IList<User> GetUsers() {
+        public IList<User> GetUsers()
+        {
             return UsersController.Users;
         }
 
         [HttpGet]
         [Route("{userId}")]
-        public User GetUser(Guid userId) {
-            return UsersController.Users.Find(user => user.Id == userId);
+        public User GetUser(Guid userId)
+        {
+            return UsersController.Users.FirstOrDefault(user => user.Id == userId);
         }
 
         [HttpPost]
         [Route("")]
-        public User CreateUser(User user) {
+        public User CreateUser(User user)
+        {
             user.Id = Guid.NewGuid();
             UsersController.Users.Add(user);
             return user;
         }
         [HttpPost]
         [Route("{userId}")]
-        public void UpdateUser(Guid userId, User user) {
-            Models.User currentUser = UsersController.Users.Find(item => item.Id==userId);
+        public void UpdateUser(Guid userId, User user)
+        {
+            Models.User currentUser = UsersController.Users.FirstOrDefault(item => item.Id == userId);
             currentUser.FirstName = user.FirstName;
             currentUser.LastName = user.LastName;
             currentUser.Age = user.Age;
             currentUser.Gender = user.Gender;
         }
-   }
+
+        [HttpDelete]
+        [Route("{userId}")]
+        public User DeleteUser(Guid userId)
+        {
+            Models.User deletedUser = UsersController.Users.FirstOrDefault(item => item.Id == userId);
+            UsersController.Users.Remove(deletedUser);
+            return deletedUser;
+
+        }
+        [HttpDelete]
+        [Route("")]
+        public IList<User> DeleteUsers(IList<Guid> userIds)
+        {
+            IList<Models.User> deletedUsers = UsersController.Users.Where(item => userIds.Contains(item.Id)).ToList();
+            foreach (Models.User item in deletedUsers)
+            {
+                UsersController.Users.Remove(item);
+            }
+            return deletedUsers;
+        }
+    }
 }
